02ac3e839c1ebc50ae1d70ce58f1fe78a8825543
Cameron Bytheway <bytheway.cameron@gmail.com>
2017-05-10 18:40:55 -0400

    return the transform object instead

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
-var transform = require('unist-util-transform');
-var normalize = transform.normalize;
+var normalize = require('unist-util-transform').normalize;
 
 module.exports = function createTransformGroup(passes) {
-  return function translator(opts) {
-    return function mapper(root, file) {
-      var instances = passes.map(function(fn) {
-        return normalize(fn(opts, file, root));
-      });
+  passes = passes.map(normalize);
 
-      return transform(root, {
-        enter: function(node, index, parent) {
-          return instances.reduce(function(node, ins) {
-            return ins.enter(node, index, parent);
-          }, node);
-        },
-        exit: function(node, index, parent) {
-          return instances.reduce(function(node, ins) {
-            return ins.exit(node, index, parent);
-          }, node);
-        }
-      });
-    };
+  return {
+    enter: function(node, index, parent) {
+      return passes.reduce(function(node, ins) {
+        return ins.enter(node, index, parent);
+      }, node);
+    },
+    exit: function(node, index, parent) {
+      return passes.reduce(function(node, ins) {
+        return ins.exit(node, index, parent);
+      }, node);
+    }
   };
 };
