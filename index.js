cffff4be8fb8e2a39996e70e9de86a3fabc456d7
Cameron Bytheway <bytheway.cameron@gmail.com>
2017-05-11 12:34:35 -0400

    bail if node becomes falsy

--- index.js
+++ index.js
@@ -8,12 +8,14 @@ module.exports = function createTransformGroup(passes) {
     enter: function(node) {
       var args = slice.call(arguments, 1);
       return passes.reduce(function(node, ins) {
+        if (!node) return node;
         return ins.enter.apply(ins, [node].concat(args));
       }, node);
     },
     exit: function(node) {
       var args = slice.call(arguments, 1);
       return passes.reduce(function(node, ins) {
+        if (!node) return node;
         return ins.exit.apply(ins, [node].concat(args));
       }, node);
     }
